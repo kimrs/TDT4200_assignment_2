c4421a0c0e044d1716e91e50f585b43488cd960e
Kim Rune Solstad <kimrs@clustis3.idi.ntnu.no>
2013-09-17 14:13:21 +0200

    modified:   jacobi.c

--- jacobi.c
+++ jacobi.c
@@ -8,7 +8,7 @@
 
 // Distribute the diverg (bs) from rank 0 to all the processes
 void distribute_diverg(){
-	printf("SIZE: ", size);
+
 }
 
 // Gather the results of the computation at rank 0
@@ -23,16 +23,25 @@ void exchange_borders(){
 
 // One jacobi iteration
 void jacobi_iteration(){
-	printf("north: %d south: %d east: %d west: %d \n", north, south, east, west);
 	
 }
 
 // Solve linear system with jacobi method
 void jacobi (int iter) {
-	
+	if(rank ==2)
+		info();	
     // Jacobi iterations
     for (int k=0; k<iter; k++) {	
 	jacobi_iteration();
     }
 
+}
+
+void info(){
+	printf("0 1 \n2 3 \n4 5 \n6 7\n\n");
+	printf("Rank: %d\n", rank);
+	printf("Coordinates: %d, %d\n\n", coords[0], coords[1]);
+	printf("Neighbours: \n");
+	printf(" %2d \n%2d %2d\n %2d", north, west, east, south);
+	printf("\n");  
 }
